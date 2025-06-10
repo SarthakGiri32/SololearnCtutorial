b6ea41dca9726ebeabfbae3d24e20b9808c733e2
Sarthak Giri <giri.sarthak1@gmail.com>
2025-06-10 21:21:43 +0530

    Add FormatSpecifiers.c and update CMakeLists.txt to include it

---
+++ FormatSpecifiers.c
@@ -0,0 +1,11 @@
+//
+// Created by Sarthak Giri on 10-06-2025.
+//
+#include <stdio.h>
+
+int main() {
+    printf("Product: %s \n", "LCD Monitor");
+    printf("Price: %f", 49.95);
+
+    return 0;
+}
