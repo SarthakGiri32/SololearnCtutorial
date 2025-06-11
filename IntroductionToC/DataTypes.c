d1d1eb78743011b4b0fe89f69ab41768f91532b3
Sarthak Giri <giri.sarthak1@gmail.com>
2025-06-11 20:01:28 +0530

    Add DataTypes.c and update CMakeLists.txt to include it

---
+++ IntroductionToC/DataTypes.c
@@ -0,0 +1,13 @@
+//
+// Created by Sarthak Giri on 11-06-2025.
+//
+#include <stdio.h>
+
+int main() {
+    char letter = 'B';
+    const int number = 127;
+
+    printf("%c%d", letter, number);
+
+    return 0;
+}
