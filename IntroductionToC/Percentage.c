7dc88eafbe2b49addb1eaabb88c134f137fd0b97
Sarthak Giri <giri.sarthak1@gmail.com>
2025-06-11 20:06:18 +0530

    Add Percentage.c and update CMakeLists.txt to include it

---
+++ IntroductionToC/Percentage.c
@@ -0,0 +1,16 @@
+//
+// Created by Sarthak Giri on 11-06-2025.
+//
+#include <stdio.h>
+
+int main() {
+    int number = 250;
+    int perc = 24;
+
+    //complete the code
+    const int result = number * perc / 100;
+
+    printf("%d", result);
+
+    return 0;
+}
