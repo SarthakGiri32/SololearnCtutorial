b2af37dc88480b2e4d3a6caba6bdb29a31644f93
Sarthak Giri <giri.sarthak1@gmail.com>
2025-06-18 19:04:45 +0530

    Add OilChange.c for oil change recommendation logic and update CMakeLists.txt

---
+++ IntroductionToC/ConditionalsAndLoops/OilChange.c
@@ -0,0 +1,22 @@
+//
+// Created by Sarthak Giri on 18-06-2025.
+//
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char input[100];
+    fgets(input, sizeof(input), stdin);
+    char *endptr;
+    int mileage = (int) strtol(input, &endptr, 10);
+    int months = (int) strtol(endptr, NULL, 10);
+
+    if (mileage > 10000 || months > 12) {
+        printf("Change Oil");
+    }
+    else {
+        printf("OK");
+    }
+
+    return 0;
+}
