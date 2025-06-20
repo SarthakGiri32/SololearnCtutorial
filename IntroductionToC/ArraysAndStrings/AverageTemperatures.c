709eff1f701ab21b3d85b69c4727dba357c66b39
SarthakGiri32 <giri.sarthak1@gmail.com>
2025-06-20 21:38:27 +0530

    Add AverageTemperatures.c for temperature retrieval functionality and update CMakeLists.txt

---
+++ IntroductionToC/ArraysAndStrings/AverageTemperatures.c
@@ -0,0 +1,14 @@
+//
+// Created by sarthakgiri on 20/6/25.
+//
+#include <stdio.h>
+
+int main() {
+    const float t[] = {10.3, 15.4, 21.8, 22.5, 25.1, 27.4, 28.8, 27.9, 20.1, 22.0, 19.4, 14.2};
+    int tempIndex;
+    scanf("%d", &tempIndex);
+
+    printf("%f", t[tempIndex]);
+
+    return 0;
+}
